79c21bad8fd266a5f6ed9de12ad457348f7da330
Nataliia K <nbezverkhaya@gmail.com>
2025-10-02 19:05:06 +0200

    test_single_name success

--- test_nationalize.py
+++ test_nationalize.py
@@ -8,3 +8,17 @@ def _validate_response(data) -> list[NationalityPrediction]:
     except ValidationError as e:
         pytest.fail(f"Pydentic validation failed: {e}")
     return vr.root
+
+def test_single_name_batch_ussage_success(api_client, base_url):
+    name = "michael"
+    params = {"name":name}
+    r = api_client.get(base_url, params=params)
+    assert r.status_code == 200
+
+    items = _validate_response(r.json())
+    assert len(items) == 1
+    assert items[0].name == name
+
+    assert isinstance(items[0].country, list)
+    for c in items[0].country:
+        assert isinstance(c, Country)
