f0475bdec4e4af5468b986b2d24e9199595001e6
Nataliia K <nbezverkhaya@gmail.com>
2025-10-02 18:29:33 +0200

    validate_response added

--- test_nationalize.py
+++ test_nationalize.py
@@ -2,3 +2,9 @@ import pytest
 from pydantic import ValidationError
 from models import NationalityResponse, NationalityPrediction, Country
 
+def _validate_response(data) -> list[NationalityPrediction]:
+    try:
+        vr = NationalityResponse.model_validate(data)
+    except ValidationError as e:
+        pytest.fail(f"Pydentic validation failed: {e}")
+    return vr.root
