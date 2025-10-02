228b3ddf2c2d2dca82bba0a8334e77ae4f6b407e
Nataliia K <nbezverkhaya@gmail.com>
2025-10-02 16:19:48 +0200

    class NationalityResponse added

--- models.py
+++ models.py
@@ -11,3 +11,11 @@ class NationalityPrediction(BaseModel):
     name:str
     country: List[Country]
 
+class NationalityResponse(BaseModel):
+    __root__: List[NationalityPrediction]
+    def __iter__(self):
+        return iter(self.__root__)
+    def __getitem__(self, item):
+        return self.__root__[item]
+    def __len__(self):
+        return len(self.__root__)
